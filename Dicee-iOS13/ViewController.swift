dfa08c4090617bd3d60eb66eeba3dabf35a86a1a
CoderSamz <codersamz-dev@outlook.com>
2022-07-25 23:11:34 +0800

    41. Storing Data using Variables and Arrays 排序骰子图片数据

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,17 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // change image
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // change image
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceThree")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFive")
+        diceImageViewOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageViewTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        if leftDiceNumber >= 6 {
+            leftDiceNumber = 0
+        }
+        
+        rightDiceNumber -= 1
+        if rightDiceNumber < 0 {
+            rightDiceNumber = 5
+        }
     }
     
 }
